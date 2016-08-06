d8bb9fccd792be2b7f0471bc6c83344d8c95772a
Pimpila Howe <pimpila@gmail.com>
2016-08-06 17:44:32 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,31 +6,28 @@ end
 
 # Define your WIN_COMBINATIONS constant
 
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 WIN_COMBINATIONS = [
-  [0, 1, 2],#WIN_COMBINATIONS[0] win_combination[]
-  [3, 4, 5],#WIN_COMBINATIONS[1]
-  [6, 7, 8],#WIN_COMBINATIONS[2]
-  [0, 3, 6],#WIN_COMBINATIONS[3]
-  [1, 4, 7],#WIN_COMBINATIONS[4]
-  [2, 5, 8],#WIN_COMBINATIONS[5]
-  [0, 4, 8],#WIN_COMBINATIONS[6]
-  [2, 4, 6] #WIN_COMBINATIONS[7]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ]
 
 def won?(board)
       WIN_COMBINATIONS.each do |win_combination|
-        win_index_1 = win_combination[0]
-        win_index_2 = win_combination[1]
-        win_index_3 = win_combination[2]
 
-        position_1 = board[win_index_1]
-        position_2 = board[win_index_2]
-        position_3 = board[win_index_3]
+        position_1 = board[win_combination[0]]
+        position_2 = board[win_combination[1]]
+        position_3 = board[win_combination[2]]
 
         if
-          position_1 == "X" ||"O" && position_2 == "X" || "O" && position_3 == "X" || "O"
+          position_1 == "X" || position_1 == "O" && position_2 == "X" || position_1 == "O" && position_3 == "X" || position_1 == "O"
           return win_combination
         else
           return false
