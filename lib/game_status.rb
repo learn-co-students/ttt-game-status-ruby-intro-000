9a861717d76e34d7d7f9c8792d5c4d5e7c709afd
Josh Tane <josh.tane@gmail.com>
2016-08-18 22:51:02 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,8 +25,9 @@ def won?(board)
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
 
-      (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-
+      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        win_combination
+      end
     end
 end
 
@@ -52,17 +53,7 @@ end
 
 def winner(board)
   if over?(board)
-    WIN_COMBINATIONS.detect do |win_combination|
-      win_index_1 = win_combination[0]
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
-
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-
-      (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      return board[win_index_1]
-    end
+    token = won?(board)
+    board[token[0]]
   end
 end
