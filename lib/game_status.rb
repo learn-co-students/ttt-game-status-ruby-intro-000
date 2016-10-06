e6d769a820358c74d9da205c6185d173965173ba
kurt cruickshank <fect11@netzero.net>
2016-10-06 00:37:55 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,61 +14,58 @@ WIN_COMBINATIONS = [
   [2,4,6]
   ]
 
+
 def won?(board)
   WIN_COMBINATIONS.each do|win|
-    if board[0]=="X" && board[1]=="X" && board[2]=="X"
-      return [0,1,2]
-      elsif board[3]=="X" && board[4]=="X" && board[5]=="X"
-        return [3,4,5]
-        elsif board[6]=="X" && board[7]=="X" && board[8]=="X"
-          return [6,7,8]
-          elsif board[0]=="O" && board[3]=="O" && board[6]=="O"
-            return [0,3,6]
-            elsif board[1]=="O" && board[4]=="O" && board[7]=="O"
-              return [1,4,7]
-              elsif board[2]=="O" && board[5]=="O" && board[8]=="O"
-                return [2,5,8]
-                elsif board[0]=="X" && board[4]=="X" && board[8]=="X"
-                  return [0,4,8]
-                  elsif board[2]=="O" && board[4]=="O" && board[6]=="O"
-                    return [2,4,6]
-                    elsif board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-                      return false
-                      #  elsif board[0]==" "&& board[1]==" "&& board[2]==" "&&, " ", " ", " ", " ", " ", " "]
-                      else
-                      end
-                    end
-                  end
+
+    win_index_1 = win[0]
+    win_index_2 = win[1]
+    win_index_3 = win[2]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win
+      elsif  position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win
+      end
+    end
+    return false
+  end
+
 
 def full?(board)
-  if board ==["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-     return true
-     else
-     end
-   end
+  board.all?{|token| token == "X" || token == "O"}
+end
+
 
 def draw?(board)
-  if board ==["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+  if full?(board) && !won?(board)
     return true
-    else
-    end
+  else
+    return false
   end
+end
+
 
 def over?(board)
-  if board ==["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-    return true
-  elsif board ==["X", "O", "X", "O", "X", "X", "O", "O", "X"]
-    return true
-    else
-    end
-  end
+  draw?(board)||board
+  full?(board)
+end
 
 def winner(board)
-  if board ==["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-    return "X"
-  elsif board ==["X", "O", " ", " ", "O", " ", " ", "O", "X"]
-    return "O"
-    else
-    end
-  end
-  
+  WIN_COMBINATIONS.each do |win_combination|
+   win_index_0 = win_combination[0]
+   win_index_1 = win_combination[1]
+   win_index_2 = win_combination[2]
+
+   if board[win_index_0] == "X" && board[win_index_1] == "X" && board[win_index_2] == "X"
+     return "X"
+   elsif board[win_index_0] == "O" && board[win_index_1] == "O" && board[win_index_2] == "O"
+     return "O"
+   elsif !won?(board)
+     return nil
+   end
+ end
+end
