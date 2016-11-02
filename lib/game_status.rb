4c764a505855557778723337c1aaa0b10670d88f
Derrik  Hanson <dh75021@gmail.com>
2016-11-02 07:06:00 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,60 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+def winning_set?(board, set, value) #array of position indices to test
+  set.all? { |e| board[e] == value }
+end
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  icons = ["X", "O"]
+  icons.each do |icon|
+    WIN_COMBINATIONS.each do |set|
+      if
+        winning_set?(board, set, icon)
+        return set
+      end
+    end
+  end
+return false
+end
+
+def full?(board)
+  if (board.include?(" ")) || board.include?(nil)
+    return false
+  else
+    return true
+  end
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  return  (won?(board) ? board[won?(board)[0]] : nil)
+
+end
