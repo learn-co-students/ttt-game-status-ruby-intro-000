190e42832943ad7a494ce9863f4d8f07c63b7ce4
sutukh19 <cy83rg33k19@outlook.com>
2017-01-08 10:25:48 -0500

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,7 +3,13 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = [" "," "," "," "," "," "," "," "," "]
+
+board.each do |item|
+    if item == ""
+        return false
+    end
+end
 
 # Define your WIN_COMBINATIONS constant
 #
@@ -18,9 +24,54 @@ WIN_COMBINATIONS = [
 	[2,4,6]
 ]
 
-top_row_win = [0,1,2]
+winner_val =
 
-if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-	  "X won in the top row"
+def won?(board)
+    WIN_COMBINATIONS.each do | wins |
+        if ((board[wins[0]] == "X" && board[wins[1]] == "X" && board[wins[2]] == "X") ||
+           (board[wins[0]] == "O" && board[wins[1]] == "O" && board[wins[2]] == "O"))
+           winner_val = board[wins[0]]
+        return wins
+        else
+            false
+        end
+     end
+       return false
 end
+
+def full?(board)
+    if board.select {|item| item == " "} != []
+        return false
+    else
+        return true
+    end
+end
+
+def draw?(board)
+    if full?(board) == true && won?(board) == false
+        return true
+    else
+        return false
+    end
+end
+
+def over?(board)
+    if won?(board) != false || draw?(board) == true
+        return true
+    else
+        return false
+    end
+end
+
+
+def winner(board)
+  if won?(board) == false
+    return nil
+  elsif board[won?(board)[0]] == "X"
+    return "X"
+  elsif board[won?(board)[0]] == "O"
+    return "O"
+  end
+end
+
 
