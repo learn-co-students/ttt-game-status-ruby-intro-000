406271df78e1b8b4b6d6bf8296512ab1619b9245
Abdullah Alam <abdullah.alam.omi@gmail.com>
2017-03-08 19:58:16 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,68 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  # ETC, an array for each win combination
+  [6,7,8], # Bottom row
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+def won?(board)
+  winner = nil
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    else
+      false
+    end
+  end
+  return winner
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if draw?(board) || won?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if(draw?(board) || !full?(board)) && !won?(board)
+    return nil
+  elsif(board[won?(board)[0]] == "X")
+    return "X"
+  elsif(board[won?(board)[0]] == "O")
+    return "O"
+  end
+end
