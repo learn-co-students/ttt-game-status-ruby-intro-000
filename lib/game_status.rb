97c7fc70885e5de8beef32859571f6690dd150ea
Roman Johnson <roman.johnson@gmail.com>
2017-06-07 19:33:22 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,9 +16,15 @@ WIN_COMBINATIONS = [
     ]
 
 def won(board)
-  if #won
-    true
-  else
-    return false
-  end
+WIN_COMBINATIONS.each do |win_combination| 
+  win_combination.each do |win_index|
+
+position_1 = board[win_index_1]
+position_2 = board[win_index_2]
+position_3 = board[win_index_3]
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
+    end
 end
