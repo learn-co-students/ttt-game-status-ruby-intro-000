09aa6b416122b5f01354e533bde5bf3f6f2919b2
shivani brijmohan <sbrij001@yahoo.com>
2018-11-21 20:24:40 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,17 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+#Create a nested array of win combinations defined in a constant
+#Tic Tac Toe has 8 possible ways to win: 3 horizontal rows, 3 vertical columns,
+#and 2 diagonals
+
+WIN_COMBINATIONS =[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+]
