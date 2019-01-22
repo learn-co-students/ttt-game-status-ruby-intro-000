6be88939d1fe8e099dda9a93e6d1005001e34fe3
funny-reference-4499 <davidbernier91@gmail.com>
2019-01-22 15:45:11 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,7 +2,7 @@
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
 
 def won?(board)
-WIN_COMBINATIONS.detect do |combo|
+WIN_COMBINATIONS.find do |combo|
     board[combo[0]] == board[combo[1]] &&
 		board[combo[1]] == board[combo[2]] &&
 		board[combo[0]] != " "
