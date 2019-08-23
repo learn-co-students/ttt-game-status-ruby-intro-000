c34b8df17eb12900d38d57efa1ad11fe61dfbadc
Naveed Ganjani <nganjani@protonmail.com>
2019-08-23 15:45:50 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,16 +16,57 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo|
-      combo.each do |index|
-          win_index = combo[index]
-          position = board[win_index]
+    win_index_1 = combo[0]
+    win_index_2 = combo[1]
+    win_index_3 = combo[2]
 
-        if position == "X" || position == "O"
-          return combo
-        else
-          return false
-        end
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
-      end
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return combo
     end
+
+  end
+  return false
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+
+def draw?(board)
+  return true if !won?(board) && full?(board)
+end
+
+def over?(board)
+  return true if won?(board) || full?(board) || draw?(board)
+end
+
+def winner(board)
+  if won?(board)
+    WIN_COMBINATIONS.each do |combo|
+      win_index_1 = combo[0]
+      win_index_2 = combo[1]
+      win_index_3 = combo[2]
+
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return "X"
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return "O"
+      end
+
+    end
+  else
+    return nil
+  end
 end
