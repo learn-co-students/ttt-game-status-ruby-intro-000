fbe2f4d94d4ab56c3de5409ed46ac38ff88cda25
Benjamin Cohn <cohnbj13@gmail.com>
2019-12-30 02:18:58 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,38 +10,42 @@ WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6
 #WIN_COMBINATIONS=[[0,4,8],[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6]]
 #WIN_COMBINATIONS=[[0,1,2],[0,4,8],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6]]
 def won?(board)
-  #if board.include?("X") || board.include?("O")
-  WIN_COMBINATIONS.each do |win_combo|
-    #binding.pry    #sets each variable in WIN_COMBINATIONS to a single variable
-    if (board[win_combo[0]] == "X" && \
-      board[win_combo[1]] == "X" && board[win_combo[2]] == "X") \
-      || (board[win_combo[0]]=="O" \
-      && board[win_combo[1]]=="O" && board[win_combo[2]]=="O")
-      return win_combo
+  if board.include?("X") || board.include?("O")
 
-    elsif board.include?("X") || board.include?("O")
-      count=0
-      board.each do |position|
-
-        if position=="X" || position=="O"
-          count+=1
-        end
-      end
-
+    WIN_COMBINATIONS.each do |win_combo|
+      binding.pry #sets each variable in WIN_COMBINATIONS to a single variable
+      if (board[win_combo[0]] == "X" && \
+        board[win_combo[1]] == "X" && board[win_combo[2]] == "X") \
+        || (board[win_combo[0]]=="O" \
+        && board[win_combo[1]]=="O" && board[win_combo[2]]=="O")
         #binding.pry
-      if count==board.length()
-        return false
-      end
-    else
-      count=0
-      board.each do |position|
-        if position==" "
-          count+=1
+        return win_combo
+
+      else
+        count=0
+        board.each do |position|
+
+          if position=="X" || position=="O"
+            count+=1
+          end
         end
+
+          #binding.pry
+        if count==board.length()
+          return false
+        end #ends the if
+      end #ends the else
+    end #ends the loop
+
+  else
+    count=0
+    board.each do |position|
+      if position==" "
+        count+=1
       end
-      if count==board.length()
-        return false
-      end
+    end
+    if count==board.length()
+      return false
     end
   end
 end
