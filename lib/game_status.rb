3a5f043777a7d872677aed5690e4bc294e7c0fa1
William Lin <willlin1996@gmail.com>
2020-01-21 17:43:43 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,14 +2,13 @@
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
-
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0, 1, 2], #top row win
 [3, 4, 5], #mid row win
 [6, 7, 8], #bottom row win
 [1, 4, 7], #mid column win
 [2, 5, 8], #right column win
-[0, 3, 6], #left column
+[0, 3, 6], #left column win
 [0, 4, 8], #left top to bottom right diagonal win
 [2, 4, 6]] #right top to bottom left diagonal win]
 
@@ -39,17 +38,18 @@ def over?(board)
     return true
   end
 end
-
+    
 def winner(board)
-  WIN_COMBINATIONS.detect do |win_conditions|
-  if board.draw?
-    return nil
-    elsif board.won? && board[win_conditions[0]] == board[win_conditions[1]] && board[win_conditions[1]] == board[win_conditions[2]] && board[win_conditions[0]] == "X"
-    "X"
-  elsif board.won? && board[win_conditions[0]] == board[win_conditions[1]] && board[win_conditions[1]] == board[win_conditions[2]] && board[win_conditions[0]] == "O"
-    "O"
+  winning_board = won?(board)
+  if winning_board != false
+    position = winning_board[0]
+  if board[position] == "X"
+    return "X"
   else
-    return nil
-  end
-  end
+    return "O"
 end
+end
+end
+    
+    
+  
