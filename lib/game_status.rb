e237a6498e17bcdce890797dabe0b718a7143feb
Don Namgyal <dhondup.namgyal@gmail.com>
2020-03-05 21:31:43 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,72 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
+    end
+  end
+  if full?(board) == false
+    return false
+  end
+end
+
+def full?(board)
+  board_full = true
+  board.each do |item|
+    index = board.index(item)
+    if position_taken?(board, index) == false
+      board_full = false
+    end
+  end
+  return board_full
+end
+
+def draw?(board)
+
+  if full?(board) && !won?(board)
+    return true
+  elsif !full?(board)&& !won?(board)
+    return false
+  elsif won?(board)
+    return false
+  else
+    return true
+  end
+end
+
+
+def over?(board)
+  if won?(board) != false || draw?(board) == true || full?(board) == true
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board) != false
+    win_combination = won?(board)
+    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+      return "X"
+    else board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
+      return "O"
+    end
+  else
+    return nil
+ end
+end
