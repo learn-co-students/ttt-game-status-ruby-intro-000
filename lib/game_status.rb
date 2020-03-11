aee349fc020553e4516e89783d4002896e34fab7
Nguyen Van Say <vansay@hotmail.fr>
2020-03-11 12:39:18 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,67 @@
+require "pry"
+
 # Helper Method
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+]
+
+def won?(board)
+
+WIN_COMBINATIONS.each do |win_combination|
+
+  position_1 = board[win_combination[0]]
+  position_2 = board[win_combination[1]]
+  position_3 = board[win_combination[2]]
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+    return win_combination
+  else
+    false
+  end
+  end
+  if full?(board) == false
+    return false
+  end
+end
+
+def full?(board)
+  board.all? do |spot|
+    spot == "X" || spot == "O"
+  end
+end
+
+def draw?(board)
+
+  if won?(board)
+    false
+  elsif full?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board)
+  row_win = won?(board)
+  board[row_win[0]]
+end
+end
+
+board = ["X", "O", " ", " ", "O", " ", " ", "O", "X"] #=> O
+board_1 = ["X", "O", " ", " ", " ", " ", " ", "O", "X"] #=> nil
+p winner(board)
