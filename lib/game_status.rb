06c1b990cfc498b8c6925659e0b94426547bd69c
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-04-01 00:28:03 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,75 @@
-# Helper Method
+# Helper Method takes a board and an index as arguments and
+# returns true or false based on whether that position on the board has been filled
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
+def won?(board)
+ WIN_COMBINATIONS .each do |win_combination|
+   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+    # grab each index from the win_combination that composes a win.
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1] # load the value of the board at win_index_1
+    position_2 = board[win_index_2] # load the value of the board at win_index_2
+    position_3 = board[win_index_3] # load the value of the board at win_index_3
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
+    end
+ end
+end
+
+def full?(board) #return true if every element in the board contains either an "X" or an "O"
+  board.all? { |index| index == "X" || index == "0" }
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif !won?(board) && !full?(board)
+    return false
+  else won?(board)
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+#  index = []
+#  index = won?(board)
+#  if index == false
+#    return nil
+#  else
+#    if board[index[0]] == "X"
+#      return "X"
+#    else
+#      return "O"
+#    end
+  end
+end
