4db6b8e018ab0e523d27f51531a7a210f451fc0a
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-04-01 02:29:02 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,27 +17,15 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
- WIN_COMBINATIONS .each do |win_combination|
-   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-    # grab each index from the win_combination that composes a win.
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1] # load the value of the board at win_index_1
-    position_2 = board[win_index_2] # load the value of the board at win_index_2
-    position_3 = board[win_index_3] # load the value of the board at win_index_3
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination # return the win_combination indexes that won.
-    else
-      false
-    end
- end
+ WIN_COMBINATIONS.detect do |combo|
+   board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
 end
 
 def full?(board) #return true if every element in the board contains either an "X" or an "O"
-  board.all? { |index| index == "X" || index == "0" }
+  board.all? { |index| index == "X" || index == "O" }
 end
 
 def draw?(board)
