0d492a45612d7979269dec3a6d449a705cf022fa
Akil Griffiths <akilgg@gmail.com>
2020-04-17 21:24:05 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,26 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [0,4,8],
+  [1,4,7],
+  [2,4,6],
+  [2,5,8]
+
+]
+
+won?(board)
+WIN_COMBINATIONS.each |array|
+board.select "X"
+board.select "O"
+if board.select "X" == array || board.select "O" == array
+  array
+else
+falsey
+end
+end
+end
