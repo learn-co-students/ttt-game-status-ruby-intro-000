2b21f5dacc7bb60cf880cf39165a4075f2011ca2
Natalia Galan <n8galan@gmail.com>
2020-06-14 06:40:05 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,68 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+[0,1,2], # top_row
+[3,4,5], # middle_row
+[6,7,8], # bottom_row
+[0,3,6], # left_column
+[1,4,7], # middle_column
+[2,5,8], # right_column
+[6,4,2], # right_diagonal
+[0,4,8] # left_diagonal
+]
+
+
+def won?(board)
+WIN_COMBINATIONS.each do |win_combination|
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+  position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination # return the win_combination indexes that won.
+    else
+      false
+     end
+ end
+
+    if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+      return false
+  end
+    won(board)
+  end
+
+  def full?(board)
+     board.all? {|i| i == "X" || i == "O"}
+  end
+
+  def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif !won?(board) && !full?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
+
+def over?(board)
+  if draw?(board) || won?(board) || full?(board)
+    return true
+  else
+    return false
+  end
+end
+
+
+def winner(board)
+   if won?(board)
+      return board[won?(board)[0]]
+   end
+end
